607bad554c372ad4e66013c64b2267abe6d0e2f4
omarmahmouddev82 <omarmahmoudr096@gmail.com>
2026-09-17 04:41:28 +0300

    feat: complete explore, cart, and favorite screens with modular widgets

--- lib/screens/button_nav_bar_screen.dart
+++ lib/screens/button_nav_bar_screen.dart
@@ -19,7 +19,7 @@ class _ButtonNavBarScreenState extends State<ButtonNavBarScreen> {
     ShopScreen(),
     ExploreScreen(),
     CartScreen(),
-    FavotireScreen(),
+    FavoriteScreen(),
     AccountScreen(),
   ];
 
